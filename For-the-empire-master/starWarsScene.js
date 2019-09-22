b97989c3db54c9fff2fb683faab3726abd718409
Bsilvs95 <brian9550@gmail.com>
2019-09-22 15:51:51 -0400

    homework

---
+++ For-the-empire-master/starWarsScene.js
@@ -0,0 +1,49 @@
+function spotEachRebel(rebels) {
+  var spottedRebelCalls = []
+
+  for (i = 0; i < rebels.length; i++) {
+    spottedRebelCalls.push('Look! Rebel scum!')
+  }
+
+  return spottedRebelCalls
+}
+
+function seeRebelsEscape(rebels) {
+  var escapedRebelCalls = []
+  var i = 0
+
+  /*do {
+    escapedRebelCalls.push('Oh no! They\'re getting away!')
+    i++ */
+  while (rebels[i] && rebels[i]['plotArmor']) {
+    escapedRebelCalls.push('Oh no! They\'re getting away!')
+    i++
+  }
+
+  return escapedRebelCalls
+}
+
+function shootAtNothing() {
+  var shotsAtNothing = []
+
+  for (var i = 0; i < 4; i++) {
+    shotsAtNothing.push('pew')
+  }
+
+  return shotsAtNothing
+}
+
+module.exports.deliverStormTroopersEscapeScene = function (rebels) {
+  var sceneSequence = []
+  console.log(rebels)
+  var spottedRebelCalls = spotEachRebel(rebels)
+  sceneSequence = sceneSequence.concat(spottedRebelCalls)
+
+  var escapedRebelCalls = seeRebelsEscape(rebels)
+  sceneSequence = sceneSequence.concat(escapedRebelCalls)
+
+  var shotsAtNothing = shootAtNothing()
+  sceneSequence = sceneSequence.concat(shotsAtNothing)
+
+  return sceneSequence
+}
